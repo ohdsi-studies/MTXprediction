0cee730ececc23e9f1c4fdf8b7a78cf8fae070b4
asaelmans <156896035+asaelmans@users.noreply.github.com>
2025-10-02 14:19:43 +0200

    Update createAnalysis.R

--- extras/createAnalysis.R
+++ extras/createAnalysis.R
@@ -1,1 +1,48 @@
+# code to create analysis script
+library(Strategus) # need https://github.com/OHDSI/Strategus/tree/v1.0-plpv-mt-modules branch
+
+#====================================================================================
+#COHORT IDS
+#====================================================================================
+
+targetIds <- c(21773, 21769, 21770, 21772,21771)
+outcomeIds <- c(21803, 21804, 21805, 21806, 21669, 21670, 21671, 21672, 21673, 21674, 21675, 21676, 21677, 21678, 21774, 21775, 21776, 21777, 21778, 21779, 21780, 21781, 21782, 21783, 21784, 21789, 21790, 21791, 21792, 21793, 21794, 21795, 21796, 21797, 21799, 21800, 21801, 21802)
+
+library(Strategus)
+ROhdsiWebApi::authorizeWebApi(
+  baseUrl = Sys.getenv('baseUrl'), 
+  authMethod = 'windows', 
+  webApiUsername = keyring::key_get('webApiUsername', 'all'), 
+  webApiPassword =  keyring::key_get('webApiPassword', 'all')
+    )
+
+# get the cohort ids from ATLAS used as the target, outcome and predictors
+cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(
+  cohortIds = c(targetId, outcomeId, predictorIds), 
+  generateStats = T,
+  baseUrl = Sys.getenv('baseUrl')
+  )
+
+#=========================================================================================
+#COHORT DIAGNOSTICS SPECIFICATIONS
+#========================================================================================
+
+cdModuleSettingsCreator <- CohortDiagnosticsModule$new()
+cdModuleSpecifications <- cdModuleSettingsCreator$createModuleSpecifications(
+  runInclusionStatistics = TRUE,
+  runIncludedSourceConcepts = TRUE,
+  runOrphanConcepts = TRUE,
+  runTimeSeries = FALSE,
+  runVisitContext = TRUE,
+  runBreakdownIndexEvents = TRUE,
+  runIncidenceRate = TRUE,
+  runCohortRelationship = TRUE,
+  runTemporalCohortCharacterization = TRUE
+)
+
+#============================================================================================
+#COHORT GENERATOR
+#============================================================================================
+
+
 
