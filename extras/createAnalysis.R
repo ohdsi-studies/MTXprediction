ae275953c3bc4528a6caa6650c1ab2f9fe593049
asaelmans <156896035+asaelmans@users.noreply.github.com>
2025-11-10 14:39:12 +0100

    Update createAnalysis.R

--- extras/createAnalysis.R
+++ extras/createAnalysis.R
@@ -7,9 +7,8 @@ library(CohortIncidence)
 #====================================================================================
 
 targetIds <- c(21773, 21769, 21770, 21772,21771)
-outcomeIds <- c(21803, 21804, 21805, 21806, 21669, 21670, 21671, 21672, 21673, 21674, 21675, 21676, 21677, 21678, 21774, 21775, 21776, 21777, 21778, 21779, 21780, 21781, 21782, 21783, 21784, 21789, 21790, 21791, 21792, 21793, 21794, 21795, 21796, 21797, 21799, 21800, 21801, 21802)
+outcomeIds <- c(21803, 21804, 21805, 21806,21669, 21670, 21671, 21672, 21673, 21674, 21675, 21676, 21677, 21678, 21774, 21775, 21776, 21777, 21778, 21779, 21780, 21781, 21782, 21783, 21784, 21789, 21790, 21791, 21792, 21793, 21794, 21795, 21796, 21797, 21799, 21800, 21801, 21802, 22003)
 
-library(Strategus)
 ROhdsiWebApi::authorizeWebApi(
   baseUrl = Sys.getenv('baseUrl'), 
   authMethod = 'windows', 
@@ -58,67 +57,37 @@ cgModuleSpecifications <- cgModuleSettingsCreator$createModuleSpecifications()
 #CohortIncidence
 #===============================================================================================
 
-# Give a name to the outcome ID set 
-oList <- cohortDefinitionSet %>%
-  filter(.data$cohortId %in% outcomeIds) %>%
-  mutate(outcomeCohortId = cohortId, outcomeCohortName = cohortName) %>%
-  select(outcomeCohortId, outcomeCohortName) %>%
-  mutate(cleanWindow = 0)
-
-#TAR
-timeAtRisks <- tibble(
-  riskWindowStart  = 0,
-  startAnchor = 'cohort start',
-  riskWindowEnd  = 365,
-  endAnchor = 'cohort end'
-)
-
 ciModuleSettingsCreator <- CohortIncidenceModule$new()
 
-tcIds <- cohortDefinitionSet %>%
-  filter(!cohortId %in% oList$outcomeCohortId & isSubset) %>%
-  pull(cohortId)
-targetList <- lapply(
-  tcIds,
-  function(cohortId) {
-    CohortIncidence::createCohortRef(
-      id = cohortId, 
-      name = cohortDefinitionSet$cohortName[cohortDefinitionSet$cohortId == cohortId]
-    )
-  }
-)
-outcomeList <- lapply(
-  seq_len(nrow(oList)),
-  function(i) {
-    CohortIncidence::createOutcomeDef(
-      id = i, 
-      name = cohortDefinitionSet$cohortName[cohortDefinitionSet$cohortId == oList$outcomeCohortId[i]], 
-      cohortId = oList$outcomeCohortId[i], 
-      cleanWindow = oList$cleanWindow[i]
-    )
-  }
+targets <- list(
+  CohortIncidence::createCohortRef(id=21773,name="Inflammatory diseases"), 
+  CohortIncidence::createCohortRef(id=21769,name="Psoriasis"), 
+  CohortIncidence::createCohortRef(id=21770,name="Atopic dermatitis"), 
+  CohortIncidence::createCohortRef(id=21772,name="Rheumatoid arthritis"),
+  CohortIncidence::createCohortRef(id=21771,name="Psoriatic arthritis") 
 )
 
-tars <- list()
-for (i in seq_len(nrow(timeAtRisks))) {
-  tars[[i]] <- CohortIncidence::createTimeAtRiskDef(
-    id = i, 
-    startWith = gsub("cohort ", "", timeAtRisks$startAnchor[i]), 
-    endWith = gsub("cohort ", "", timeAtRisks$endAnchor[i]), 
-    startOffset = timeAtRisks$riskWindowStart[i],
-    endOffset = timeAtRisks$riskWindowEnd[i]
+outcomes <- list(
+  CohortIncidence::createOutcomeDef(id=1,name="Drugscombined",cohortId=21803),
+  CohortIncidence::createOutcomeDef(id=2,name="Psoriasisdrugs",cohortId=21804),
+  CohortIncidence::createOutcomeDef(id=3,name="Atopicdermatitisdrugs",cohortId=21805),
+  CohortIncidence::createOutcomeDef(id=4,name="RA&PAdrugs",cohortId=21806)
+)
+
+tars <- list(
+  CohortIncidence::createTimeAtRiskDef(id=1,startWith="start", startOffset = 0, endWith="end", endOffset = 183), 
+  CohortIncidence::createTimeAtRiskDef(id=2,startWith="start", startOffset = 0, endWith="end",endOffset=365) 
   )
-}
-analysis1 <- CohortIncidence::createIncidenceAnalysis(
-  targets = tcIds,
-  outcomes = seq_len(nrow(oList)),
-  tars = seq_along(tars)
-)
 
+analysis1 <- CohortIncidence::createIncidenceAnalysis(
+  targets = c(21773,21769,21770,21772,21771),
+  outcomes = c(1,2,3,4),
+  tars = c(1,2)
+)
 
 irDesign <- CohortIncidence::createIncidenceDesign(
-  targetDefs = targetList,
-  outcomeDefs = outcomeList,
+  targetDefs = targets,
+  outcomeDefs = outcomes,
   tars = tars,
   analysisList = list(analysis1),
   #studyWindow = irStudyWindow,
@@ -129,10 +98,6 @@ irDesign <- CohortIncidence::createIncidenceDesign(
     ageBreaks = seq(0, 110, by = 10)
   )
 )
-cohortIncidenceModuleSpecifications <- ciModuleSettingsCreator$createModuleSpecifications(
-  irDesign = irDesign$toList()
-)
-
 
 ciModuleSpecifications <- ciModuleSettingsCreator$createModuleSpecifications(
   irDesign = irDesign$toList()
@@ -141,42 +106,154 @@ ciModuleSpecifications <- ciModuleSettingsCreator$createModuleSpecifications(
 #===============================================================================================
 #TreatmentPatterns
 #===============================================================================================
+#remotes::install_github("darwin-eu-dev/TreatmentPatterns@338")
 
 # Create dataframe for TreatmentPatterns
-cohorts <- cohortDefinitionSet %>%
+cohorts <- cohortDefinitionSet |>
   select (cohortId, cohortName)
 
+oList <- cohortDefinitionSet |>
+  filter(.data$cohortId %in% outcomeIds) |>
+  mutate(outcomeCohortId = cohortId, outcomeCohortName = cohortName) |>
+  select(outcomeCohortId, outcomeCohortName) |>
+  mutate(cleanWindow = 0)
+
 cohorts$type <- ifelse(cohorts$cohortId %in% oList$outcomeCohortId, 'event', 'target')
+
+#TREATMENTPATTERNSAD
+
+cohortsAD <- cohorts[c(3,11,26:33, 44),]
 
 # Remove certain IDs to try to overcome stuck
 #cohorts = cohorts%>%filter(cohortId>...)
 
-tpModuleSettingsCreator <- TreatmentPatternsModule$new()
-tpModuleSpecifications <- tpModuleSettingsCreator$createModuleSpecifications(
-  cohorts, 
-  includeTreatments = "startDate",
-  indexDateOffset = 0,
-  minEraDuration = 1,
+tpModuleSettingsCreatorAD <- TreatmentPatternsModule$new()
+tpModuleSpecificationsAD <- tpModuleSettingsCreatorAD$createModuleSpecifications(
+  cohorts = cohortsAD, 
+  minEraDuration = 30,
   splitEventCohorts = NULL,
   splitTime = NULL,
-  eraCollapseSize = 7,
-  combinationWindow = 1,
-  minPostCombinationDuration = 1,
-  filterTreatments = "All",
-  maxPathLength = 7,
-  ageWindow = 10,
-  minCellCount = 5,
-  censorType = "minCellCount"
+  eraCollapseSize = 30,
+  combinationWindow = 30,
+  minPostCombinationDuration = 30,
+  filterTreatments = "First",
+  maxPathLength = 5, 
+  ageWindow = 10, 
+  minCellCount = 5, 
+  censorType = "minCellCount",
+  overlapMethod = "truncate",
+  concatTargets = FALSE,
+  startAnchor = "startDate",
+  windowStart = 0,
+  endAnchor = "endDate",
+  windowEnd = 365
 )
 
+
+#TREATMENTPATTERNSP
+
+cohortsP <- cohorts[c(2, 10:25, 44),]
+
+# Remove certain IDs to try to overcome stuck
+#cohorts = cohorts%>%filter(cohortId>...)
+
+tpModuleSettingsCreatorP <- TreatmentPatternsModule$new()
+tpModuleSpecificationsP <- tpModuleSettingsCreatorP$createModuleSpecifications(
+  cohorts = cohortsP, 
+  minEraDuration = 30,
+  splitEventCohorts = NULL,
+  splitTime = NULL,
+  eraCollapseSize = 30,
+  combinationWindow = 30,
+  minPostCombinationDuration = 30,
+  filterTreatments = "First",
+  maxPathLength = 5, 
+  ageWindow = 10, 
+  minCellCount = 5, 
+  censorType = "minCellCount",
+  overlapMethod = "truncate",
+  concatTargets = FALSE,
+  startAnchor = "startDate",
+  windowStart = 0,
+  endAnchor = "endDate",
+  windowEnd = 365
+)
+
+#TREATMENTPATTERNSPA
+
+cohortsPA <- cohorts[c(5,13,14,23,24,31,32,34,35,37:44),]
+
+# Remove certain IDs to try to overcome stuck
+#cohorts = cohorts%>%filter(cohortId>...)
+
+tpModuleSettingsCreatorPA <- TreatmentPatternsModule$new()
+tpModuleSpecificationsPA <- tpModuleSettingsCreatorPA$createModuleSpecifications(
+  cohorts = cohortsPA, 
+  minEraDuration = 30,
+  splitEventCohorts = NULL,
+  splitTime = NULL,
+  eraCollapseSize = 30,
+  combinationWindow = 30,
+  minPostCombinationDuration = 30,
+  filterTreatments = "First",
+  maxPathLength = 5, 
+  ageWindow = 10, 
+  minCellCount = 5, 
+  censorType = "minCellCount",
+  overlapMethod = "truncate",
+  concatTargets = FALSE,
+  startAnchor = "startDate",
+  windowStart = 0,
+  endAnchor = "endDate",
+  windowEnd = 365
+)
+
+#TREATMENTPATTERNSRA
+
+cohortsRA <- cohorts[c(4,13,14,23,24,31,32,34,35,37:44),]
+
+# Remove certain IDs to try to overcome stuck
+#cohorts = cohorts%>%filter(cohortId>...)
+
+tpModuleSettingsCreatorRA <- TreatmentPatternsModule$new()
+tpModuleSpecificationsRA <- tpModuleSettingsCreatorRA$createModuleSpecifications(
+  cohorts = cohortsRA, 
+  minEraDuration = 30,
+  splitEventCohorts = NULL,
+  splitTime = NULL,
+  eraCollapseSize = 30,
+  combinationWindow = 30,
+  minPostCombinationDuration = 30,
+  filterTreatments = "First",
+  maxPathLength = 5, 
+  ageWindow = 10, 
+  minCellCount = 5, 
+  censorType = "minCellCount",
+  overlapMethod = "truncate",
+  concatTargets = FALSE,
+  startAnchor = "startDate",
+  windowStart = 0,
+  endAnchor = "endDate",
+  windowEnd = 365
+)
+
+#splitEventCohorts = NULL,
+#splitTime = NULL,
+#eraCollapseSize = 30,
+#overlapMethod = "truncate",
+#concatTargets = TRUE, 
+#startAnchor = "startDate",
+#windowStart = 0,
+#endAnchor = "endDate",
+#windowEnd = 0
 #===============================================================================================
 #Characterization
 #===============================================================================================
 
 cModuleSettingsCreator <- CharacterizationModule$new()
 cModuleSpecifications <- cModuleSettingsCreator$createModuleSpecifications(
-  targetIds = cohortDefinitionSet$cohortId,
-  outcomeIds = oList$outcomeCohortId,
+  targetIds = c(21773, 21769, 21770, 21772,21771),
+  outcomeIds = 21803,
   minPriorObservation = 365,
   outcomeWashoutDays = 365,
   dechallengeStopInterval = 30,
@@ -184,11 +261,10 @@ cModuleSpecifications <- cModuleSettingsCreator$createModuleSpecifications(
   riskWindowStart = 0, 
   startAnchor = 'cohort start', 
   riskWindowEnd = 365, 
-  endAnchor = 'cohort start,
+  endAnchor = 'cohort start',
   covariateSettings = FeatureExtraction::createDefaultCovariateSettings(),
   minCharacterizationMean = .01
   )
-
 
 #===============================================================================================
 #PatientLevelPrediction
@@ -198,15 +274,18 @@ cModuleSpecifications <- cModuleSettingsCreator$createModuleSpecifications(
 #ANALYSIS SPECIFICATIONS CDM MODULES
 #===============================================================================================
 
-analysisSpecifications <- createEmptyAnalysisSpecificiations() |>
+analysisSpecifications <- createEmptyAnalysisSpecifications() |>
   addSharedResources(cohortSharedResourcesSpecifications) |>
   addCohortDiagnosticsModuleSpecifications(cdModuleSpecifications) |>
   addCohortGeneratorModuleSpecifications(cgModuleSpecifications) |>
   addCohortIncidenceModuleSpecifications(ciModuleSpecifications) |>
-  addTreatmentPatternsModuleSpecifications(tpModuleSpecifications) |>
+  addTreatmentPatternsModuleSpecifications(tpModuleSpecificationsAD) |>
+  #addTreatmentPatternsModuleSpecifications(tpModuleSpecificationsP) |>
+  #addTreatmentPatternsModuleSpecifications(tpModuleSpecificationsPA) |>
+  #addTreatmentPatternsModuleSpecifications(tpModuleSpecificationsRA) |>
   addCharacterizationModuleSpecifications(cModuleSpecifications) 
 
 ParallelLogger::saveSettingsToJson(
   object = analysisSpecifications,
-  fileName = "./inst/study_execution_jsons/validation.json"
+  fileName = "C:/Users/ASaelman/MTXpredictionnetwork/inst/study_execution_jsons/validation.json"
 )
